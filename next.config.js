421774cca8daded8cb7c237d58701f2715adeeb4
Luiz Bueno <contact@luizbueno.me>
2021-06-29 00:07:09 -0300

    Correct PurgeCSS configs.

--- next.config.js
+++ next.config.js
@@ -2,19 +2,18 @@ module.exports = {
   reactStrictMode: true,
   i18n: {
     locales: ["en"],
-    defaultLocale: "en",
+    defaultLocale: "en"
   },
-  [
+  plugins: [
     "@fullhuman/postcss-purgecss",
-    process.env.NODE_ENV === "production"
-      ? {
-          content: [
-            "./pages/**/*.{js,jsx,ts,tsx}",
-            "./components/**/*.{js,jsx,ts,tsx}",
-          ],
-          defaultExtractor: (content) =>
-            content.match(/[\w-/:]+(?<!:)/g) || [],
-        }
-      : false,
+    process.env.NODE_ENV === "production" ?
+    {
+      content: [
+        "./pages/**/*.{js,jsx,ts,tsx}",
+        "./components/**/*.{js,jsx,ts,tsx}"
+      ],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || []
+    } :
+    false
   ]
 }
